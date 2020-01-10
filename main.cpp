f12ee9c43b619ffa16674e900964775a50a7875b
samzola <353754@bsd48.org>
2020-01-10 09:13:02 -0800

    delete and average working

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 void add(); //function to add a student
 void print(Node* next); //function to print list
+void average(Node* next);
+void del(Node* next, Node* previous, int id);
 
 Node* head = NULL; //create first node and set to null
 
@@ -34,38 +36,19 @@ int main() {
       running = false; //make running false, end program
     }
     else if (strcmp(command, "average") == 0) {
-      int nodeCount = 0;
-      float total = 0;
-      Node* current = head;
-      while (current != NULL) {
-	nodeCount++;
-	total += current->getStudent()->getGPA();
-	current = current->getNext();
-      }
-      cout << "Average GPA: " << total/nodeCount << fixed << setprecision(2) << endl;
+      average(head);
     }
     else if (strcmp(command, "delete") == 0) {
-      int delID = 0;
-      cout << "Please enter the ID of the student you would like to delete." << endl;
-      cin >> delID;
-      cin.clear();
-      cin.ignore(1000000, '\n');
-      Node* current = head;
-      while (current != NULL) {
-	if (head->getStudent()->getID() == delID) {
-	  cout << "Student " << head->getStudent()->getName() << " deleted." << endl;
-	  head = head->getNext();
-	}
-	else {
-	  if (current->getStudent()->getID() == delID) {
-	    Node* previous = current->getPrevious();
-	    Node* next = current->getNext();
-	    previous->setNext(next);
-	    next->setPrevious(previous);
-	    
-	  }
-	}
-	current = current->getNext();
+      int inID = 0;
+      if (head == NULL) {
+	cout << "There are no students in the list." << endl;
+      }
+      else {
+	cout << "Please enter the ID of the student you would like to delete." << endl;
+	cin >> inID;
+	cin.clear();
+	cin.ignore(1000000, '\n');
+	del(head, NULL, inID);
       }
     }
     else { //if command is invalid, tell user to try again
@@ -87,20 +70,19 @@ void add() { //function to add a student
   cout << "Please enter the student's GPA." << endl; //tell user to enter gpa
   cin >> gpa;
   cin.clear();
-  cin.ignore(1000000, '\n');
- 
+  cin.ignore(1000000, '\n'); 
   Student* newStudent = new Student(name, id, gpa); //make a new student with the entered name
   Node* current = head; //make a node, current, equal to the first node
   if (current == NULL) { //if the current node has no student
     head = new Node(newStudent); //make a node with the recently made student
     head->setStudent(newStudent); //set student of the node
   }
-  else { //if current node has a student
+  else {
     while (current->getNext() != NULL) { //when the next node has no student
       current = current->getNext(); //make current the next node
     }
-    current->setNext(new Node(newStudent)); //set the recently made student to the empty node
-    current->getNext()->setStudent(newStudent); //set student of node
+  current->setNext(new Node(newStudent)); //set the recently made student to the empty node
+  current->getNext()->setStudent(newStudent); //set student of node
   }
 }
 
@@ -108,14 +90,71 @@ void print(Node* next) { //function for printing list
   if (next == head) { //if at beginning of list
     cout << "list: "; //print out "list: "
   }
-  if (next != NULL) { //if the node has a student in it
-    //if (next->getNext() != NULL) { //if the next node has a student in it 
-      cout << next->getStudent()->getName() << ", " << next->getStudent()->getID() << ", " << next->getStudent()->getGPA() << fixed << setprecision(2) << ", "; //print name of student with comma after
+  if (next != NULL) { //if the node has a student in it 
+      cout << next->getStudent()->getName() << ", " << next->getStudent()->getID() << ", " << next->getStudent()->getGPA() << fixed << setprecision(2) << ", ";
       print(next->getNext());
-      //}
-    /*else if (next->getNext() == NULL) { //if the next node has no student
-      cout << next->getStudent()->getName() << ", " << next->getStudent()->getID() << ", " << next->getStudent()->getGPA() << fixed << setprecision(2) << " "; //print name of student w/o comma after
+  }
+}
+
+void average(Node* next) {
+  int nodeCount = 0;
+  float total = 0;
+  if (head == NULL) {
+    cout << "There are no students in the list." << endl;
+  }
+  else {
+    while (next->getNext() != NULL) {
+      cout << next->getStudent()->getGPA() << endl;
+      total += next->getStudent()->getGPA();
+      nodeCount++;
+      next = next->getNext();
     }
-    print(next->getNext()); //recursion, keep printing until end of list*/
+    total += next->getStudent()->getGPA();
+    nodeCount++;
+    float ave = (total/nodeCount);
+    cout << "The average GPA is " << fixed << setprecision(2) << ave << endl;
+  }
+}
+
+void del(Node* next, Node* previous, int id) {
+  char* choice = new char[5];
+  if (next == head && (next->getStudent()->getID() == id)) {
+    cout << endl << next->getStudent()->getName() << endl;
+    cout << "Would you like to delete this student?" << endl;
+    cin.getline(choice, 4, '\n');
+    if ((strcmp(choice, "yes") == 0) || (strcmp(choice, "y") == 0)) {
+      cout << "Student deleted." << endl;
+      if (next->getNext() == NULL) {
+	next->~Node();
+	head = NULL;
+      }
+      else {
+	head = next->getNext();
+	next->~Node();
+	del(head, NULL, id);
+      }
+    }
+    else if ((strcmp(choice, "no") == 0) || (strcmp(choice, "n") == 0)) {
+	cout << "Student not deleted." << endl;
+    }
+    else {
+      cout << "Please enter yes or no." << endl;
+    }
+  }
+  else if (next->getStudent()->getID() == id) {
+    cout << endl << next->getStudent()->getName() << endl;
+    cout << "Would you like to delete this student?" << endl;
+    cin.getline(choice, 10, '\n');
+    if ((strcmp(choice, "yes") == 0) || (strcmp(choice, "y") == 0)) {
+      cout << "Student deleted." << endl;
+      previous->setNext(next->getNext());
+      next->~Node();
+    }
+    else if ((strcmp(choice, "no") == 0) || (strcmp(choice, "n") == 0)) {
+      cout << "Student not deleted." << endl;
+    }
+    else {
+      cout << "Please enter yes or no." << endl;
+    }
   }
 }
